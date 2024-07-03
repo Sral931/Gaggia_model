18cb4e9a28c6dfc0c9a58c1702ee3f2d23a3183a
Sral931 <105347332+Sral931@users.noreply.github.com>
2024-07-03 22:14:52 +0200

    Implemented logging

--- src/functions_sim.py
+++ src/functions_sim.py
@@ -17,6 +17,7 @@ class Simulator():
         # set contruct params
         self.index_model: int32 = index_model
         self.index_solver: int32 = index_solver
+        self.logging = True
 
         # setup model
         self.num_states: int32 = np.shape(self.list_states())[0]
@@ -33,6 +34,7 @@ class Simulator():
                     'body',
                     'ambient'
                 ]
+
     def list_inputs(self) -> list:
         match self.index_model:
             case 0:
@@ -71,7 +73,7 @@ class Simulator():
             result[ind] = np.array([ 1.8/2.0, 1.8/2.0, 0.0, 0.0, 0.0, 0.0, 0.0])
             result[ind,ind] -= np.sum(result[ind][:self.num_states])
             # ambient
-            # has inf capacity due to boundary
+            # has inf capacity, since it's considered a steady boundary
             ind = 5
             result[ind] = np.array([ 0.0, 0.0, 0.0, 0.55, 0.0, 0.0, 0.0])
             result[ind,ind] -= np.sum(result[ind])
@@ -88,6 +90,7 @@ class Simulator():
     
     def initialize(self, init_state:ndarray) -> None:
         self.state = init_state
+        self.log = np.array([init_state])
 
     def solve(self, inputs: ndarray, timestep: float64) -> None:
         # check
@@ -97,12 +100,17 @@ class Simulator():
             raise ValueError('Incorrect length of inputs-array !')
         
         # main
-        self.state += np.matmul(self.system(),np.append(self.state, inputs))*timestep
+        self.state += np.matmul(self.system(), np.append(self.state, inputs)) * timestep
+        if self.logging:
+            self.log = np.append(self.log, [self.state], axis=0)
 
 if __name__ == '__main__':
     sim = Simulator(0, 0)
     sim.initialize(init_state=20.0*np.ones(sim.num_states))
-    print(sim.state)
+    # print(sim.state)
     for index in range(60*10):
         sim.solve(1350.0*np.ones(1), 0.1)
-        print(sim.state)
+        # print(sim.state)
+
+    # print(sim.log[:,0])
+    
